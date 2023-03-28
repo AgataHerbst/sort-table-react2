5ccc80f6bc423a6abc291a93fd636d7df0a1d6e0
AgataHerbst <125795402+AgataHerbst@users.noreply.github.com>
2023-03-28 23:19:48 +0300

    коррекция ошибок + изменение css

--- components/ReadOnlyRow.js
+++ components/ReadOnlyRow.js
@@ -4,10 +4,10 @@ function ReadOnlyRow ({user, handleEditClick, handleDeleteClick }) {
     <tr className={s.td} >
    <td>{user.name}</td>
    <td>{user.email}</td>
-   <td>{user.address.city}</td>
+   <td>{user.address?.city}</td>
    <td>{user.phone}</td>
    <td>{user.website}</td>
-   <td>{user.company.name}</td>
+   <td>{user.company?.name}</td>
    <td>
     <button className={s.edit} type='button' onClick={(event) => handleEditClick(event, user)}>Edit</button>
     <button className={s.delete} type='button' onClick={()=>handleDeleteClick(user.id)}>Delete</button>
--- components/Tbody.js
+++ components/Tbody.js
@@ -4,110 +4,111 @@ import { nanoid } from 'nanoid';
 import ReadOnlyRow from '../components/ReadOnlyRow';
 import EditableRow from '../components/EditableRow';
 
-function Tbody () {
-const [ users, setUsers ] = useState([]);
-const [editContactId, setEditContactId] = useState(null);
-const[ addFormData, setAddFormData ] = useState();
+function Tbody() {
+  const [users, setUsers] = useState([]);
+  const [editContactId, setEditContactId] = useState(null);
+  const [addFormData, setAddFormData] = useState();
 
 
-const handleAddFormChange = (event) => {
-  event.preventDefault();
+  const handleAddFormChange = (event) => {
+    event.preventDefault();
 
-  const fieldName = event.target.getAttribute("name");
-  const fieldValue = event.target.value;
+    const fieldName = event.target.getAttribute("name");
+    const fieldValue = event.target.value;
 
-  const newFormData = { ...addFormData };
-  newFormData[fieldName] = fieldValue;
+    const newFormData = { ...addFormData };
+    newFormData[fieldName] = fieldValue;
 
-  setAddFormData(newFormData);
-};
-
-function handleAddFormSubmit(event) {  //функция ДОБАВЛЕНИЯ контакта
-  event.preventDefault();
-
-
-  const newContact = { //добавление нового контакта
-    id: nanoid(),
-    name: addFormData.name,
-    email: addFormData.email,
-    address: addFormData.address.city,
-    phone: addFormData.phone,
-    website: addFormData.website,
-    companyName: addFormData.company.name
+    setAddFormData(newFormData);
   };
 
-  const newContacts = [...users, newContact];
-  setUsers(newContacts);
-};
+  function handleAddFormSubmit(event) {  //функция ДОБАВЛЕНИЯ контакта
+    event.preventDefault();
 
-function handleEditFormSubmit(obj) {
 
-  const newContacts = [...users];
+    const newContact = { //добавление нового контакта
+      id: nanoid(),
+      name: addFormData.name,
+      email: addFormData.email,
+      address: addFormData.address?.city,
+      phone: addFormData.phone,
+      website: addFormData.website,
+      companyName: addFormData.company?.name
+    };
 
-  const index = users.findIndex((user) => user.id === editContactId);
+    const newContacts = [...users, newContact];
+    setUsers(newContacts);
+  };
 
-  newContacts[index] = Object.assign(newContacts[index], obj);
+  function handleEditFormSubmit(obj) {
 
-  setUsers(newContacts);
-  setEditContactId(null);
-};
+    const newContacts = [...users];
 
-function handleEditClick(event, user) {
-  setEditContactId(user.id);
-}
+    const index = users.findIndex((user) => user.id === editContactId);
 
-function handleCancelClick() { //кнопка cancel
-  setEditContactId(null);
-};
+    newContacts[index] = Object.assign(newContacts[index], obj);
 
-function handleDeleteClick(userId) {
-  const newContacts = [...users];
+    setUsers(newContacts);
+    setEditContactId(null);
+  };
 
-  const index = users.findIndex((user) => user.id === userId);
-
-  newContacts.splice(index, 1);
-
-  setUsers(newContacts);
-}
-
-try {
-  const getUsers = async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const FinalData = await response.json();
-    setUsers(FinalData)
+  function handleEditClick(event, user) {
+    setEditContactId(user.id);
   }
 
-  useEffect(() => {
-    getUsers();
-  }, [])
+  function handleCancelClick() { //кнопка cancel
+    setEditContactId(null);
+  };
 
-} catch (err) {
-  console.log('Поймали ошибку.')
-}
+  function handleDeleteClick(userId) {
+    const newContacts = [...users];
 
-      return <>
- <div className={s.container}>   
- <table className={s.table}>
-        <thead>
+    const index = users.findIndex((user) => user.id === userId);
+
+    newContacts.splice(index, 1);
+
+    setUsers(newContacts);
+  }
+
+  try {
+    const getUsers = async () => {
+      const response = await fetch("https://jsonplaceholder.typicode.com/users");
+      const FinalData = await response.json();
+      setUsers(FinalData)
+    }
+
+    useEffect(() => {
+      getUsers();
+    }, [])
+
+  } catch (err) {
+    console.log('Поймали ошибку.')
+  }
+
+  return <>
+    <div className={s.container}>
+      <table className={s.table}>
+        <thead className={s.thead}>
           <tr className={s.th}>
-           <th>Name</th> 
-           <th>Email</th>
-           <th>Address.city</th>
-           <th>Website</th>
-           <th>Phone</th>
-           <th>Company.Name</th>
-           <th>Actions</th>
+            <th className={s.th}>Name</th>
+            <th className={s.th}>Email</th>
+            <th className={s.th}>Address.city</th>
+            <th className={s.th}>Website</th>
+            <th className={s.th}>Phone</th>
+            <th className={s.th}>Company.Name</th>
+            <th className={s.th}>Actions</th>
           </tr>
         </thead>
         <tbody className={s.tbody}>
 
-{users.map((user) => {
-return editContactId === user.id
+          {users.map((user) => {
+            return editContactId === user.id
               ? <EditableRow
                 key={user.id}
                 editFormData={user}
                 handleEditFormSubmit={handleEditFormSubmit}
                 handleCancelClick={handleCancelClick}
+
               />
               : <ReadOnlyRow
                 key={user.id}
@@ -116,19 +117,62 @@ return editContactId === user.id
                 handleDeleteClick={handleDeleteClick}
               />
           })}
-      <tr><td colSpan={8}><h2 className={s.h2}>Add a New Contact</h2></td></tr>
-      <EditableRow editFormData={{ name: '', email: '', address: { city: '' }, website: '', phone: '', company: { name: '' } }} />
-        
-</tbody>
-</table>
-</div>
-</>
-     
+
+          <tr><td colSpan={8}><h2 className={s.h2}>Add a New Contact</h2>
+          </td></tr>
+
+        </tbody>
+      </table>
+      <form onSubmit={handleAddFormSubmit}>
+        <input className={s.input}
+          type="text"
+          name="name"
+          required="required"
+          placeholder="Enter a name..."
+          onChange={handleAddFormChange}
+        />
+        <input className={s.input}
+          type="text"
+          name="email"
+          required="required"
+          placeholder="Enter a email..."
+          onChange={handleAddFormChange}
+        />
+        <input className={s.input}
+          type="text"
+          name="addressCity"
+          required="required"
+          placeholder="Enter an address..."
+          onChange={handleAddFormChange}
+        />
+        <input className={s.input}
+          type="text"
+          name="phone"
+          required="required"
+          placeholder="Enter a phone number..."
+          onChange={handleAddFormChange}
+        />
+        <input className={s.input}
+          type="text"
+          name="website"
+          required="required"
+          placeholder="Enter an website..."
+          onChange={handleAddFormChange}
+        />
+        <input className={s.input}
+          type="text"
+          name=" companyName:"
+          required="required"
+          placeholder="Enter an companyName..."
+          onChange={handleAddFormChange}
+        />
+        <button className={s.add} type="submit">Add</button>
+      </form>
+
+    </div>
+  </>
+
 }
 
 export default Tbody;
-
-
-
-
  
